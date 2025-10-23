cdfbe0773d87812f57d5ff73917778fb7f98baa7
samuelUPC2024 <samueldanquah10@outlook.com>
2025-10-23 12:32:45 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -128,7 +128,7 @@ def crear_features_temporales(df):
 # |                   BARRA LATERAL (SIDEBAR) (MODIFICADA)                 |
 # --------------------------------------------------------------------------
 
-logo_path = "Logo_ASEPEYO.png"
+logo_path = "LogoSAMPHEL ENERGIES.png"
 if os.path.exists(logo_path):
     st.sidebar.image(logo_path, width=200)
 else:
