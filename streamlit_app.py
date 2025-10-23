108b4a4fa31acd3c77a38de8e32c0c7c35880dc9
samuelUPC2024 <samueldanquah10@outlook.com>
2025-10-23 12:10:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ import glob  # <-- NUEVO: Para encontrar archivos en carpetas
 # |                   CONFIGURACIÓN DE LA PÁGINA                             |
 # --------------------------------------------------------------------------
 st.set_page_config(
-    page_title="Predicción de Consumo Energético con IA",
+    page_title="SAMPHEL ENERGY",
     page_icon="🤖",
     layout="wide",
     initial_sidebar_state="expanded"
